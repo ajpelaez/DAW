d128d331290a37b414b54f95fe511cc78ddf81c6
ajpelaez <bunyi@sindominio.net>
2022-05-11 19:17:06 +0200

    Arregla ejercicios

---
+++ DWEC/ejemplos_angular/MiniCalculadora/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MiniCalculadora';
+
+  number1:number = 0;
+  number2:number = 0;
+  result:number = 0;
+
+sumar():void{
+  this.result = this.number1 + this.number2;
+}
+
+restar():void{
+  this.result = this.number1 - this.number2;
+}
+
+
+}
